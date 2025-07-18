b2e9c9d1bcb43c15d5f0520a623a2062fa723c0d
Ethan Brown <88006720+swiftwarkite2@users.noreply.github.com>
2025-07-18 13:40:47 -0700

    Update twotofour_decoder.v

--- project_7/project_7.srcs/sources_1/new/twotofour_decoder.v
+++ project_7/project_7.srcs/sources_1/new/twotofour_decoder.v
@@ -3,7 +3,6 @@
 // uses the 5 mS clock to cycle the power between each of the four 7_seg displays faster the human eye can notice
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module twotofour_decoder(
 input [1:0]A,
 output reg[3:0]an
