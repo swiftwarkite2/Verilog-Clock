df154956cf0f99d690260d7a38f78bdae6a22da4
Ethan Brown <88006720+swiftwarkite2@users.noreply.github.com>
2025-07-18 13:40:04 -0700

    Update twotofour_decoder.v

--- project_7/project_7.srcs/sources_1/new/twotofour_decoder.v
+++ project_7/project_7.srcs/sources_1/new/twotofour_decoder.v
@@ -1,22 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/29/2024 04:56:42 PM
-// Design Name: 
-// Module Name: twotofour_decoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// uses the 5 mS clock to cycle the power between each of the four 7_seg displays faster the human eye can notice
 //////////////////////////////////////////////////////////////////////////////////
 
 
